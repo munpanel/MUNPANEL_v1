fe69e368a16e9e3d56da7bf25b98ff63f21a5041
Adam Yi <xuan@yiad.am>
2016-11-26 21:04:17 +0800

    Google Analytics

---
+++ config/analytics.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'enabled' => env('ANALYTICS_ENABLED', false),
+    'trackingID' => env('ANALYTICS_TRACKINGID', 'UA-88064834-1')
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,18 @@
       <script src="{{secure_url('/js/ie/excanvas.js')}}" cache="false"></script>
       <script src="{{secure_url('/js/ie/fix.js')}}" cache="false"></script>
     <![endif]-->
+@if (Config::get('analytics.enabled'))
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', "{{Config::get('analytics.trackingID')}}", 'auto');
+      ga('send', 'pageview');
+
+    </script>
+@endif
 </head>
 <body>
   <section class="hbox stretch">

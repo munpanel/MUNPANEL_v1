5f99e4c37864dc21019e46f11d5d37b310616ebe
CRH380B-6216L <muner_szr6216@outlook.com>
2016-12-12 19:48:58 +0100

    看一通之后想了想在committee表又加了点用来丰富委员会主页的东西

--- database/migrations/2016_11_18_090938_create_committees_table.php
+++ database/migrations/2016_11_18_090938_create_committees_table.php
@@ -21,7 +21,18 @@ class CreateCommitteesTable extends Migration
 			$table->string('topic_1')->nullable();
 			$table->enum('topic_sel', ['Topic0', 'Topic1', 'Unchosen']);
 			$table->enum('language', ['ChineseS', 'English']);
+			//议事规则：美规、欧规、暂规
+			//如果是经修改的规则，将rule_modified设为true，
+			//使委员会主页中议事规则一项增加“经修改的/Modified”前缀
+			$table->enum('rule', ['robert', 'eu', 'sc']);
+			$table->boolean('rule_modified')->default(false);
+			//危机推动类型：无、仅新闻（半危机推动？不需要写DD）、有、有联动
+			$table->enum('crisis', ['none', 'news_only', 'yes', 'joint']);
+			//时间节点默认取会议起止日
+			$table->date('timeframe_start');
+			$table->date('timeframe_end');
 			$table->integer('session')->unsigned();
+			$table->text('description');
             $table->timestamps()->nullable();
         });
     }

7ade4d0cda28d8d11a22e901adff46b3366e2d7c
CRH380B-6216L <muner_szr6216@outlook.com>
2017-01-01 19:59:58 +0100

    初步构建 dais 页面侧边栏

--- resources/views/layouts/dais.blade.php
+++ resources/views/layouts/dais.blade.php
@@ -4,3 +4,34 @@
                   <span>Home</span>
                 </a>
               </li>
+              <li class="@yield('roles_active')">
+                <a href="{{ secure_url('/roleAlloc') }}">
+                  <i class="fa fa-wheelchair"></i>
+                  <span>Role Allocation</span>
+                </a>
+              </li>
+              <li class="dropdown-submenu @yield('assignments_active')">
+                <a href="{{ secure_url('/assignmentManage') }}" class="dropdown-toggle" > <!-- data-toggle="dropdown"-->
+                  <i class="fa fa-flask"></i>
+                  <span>Assignments & Handins</span>
+                </a>
+              </li>
+              <li class="@yield('documents_active')">
+                <a href="{{ secure_url('/documents') }}">
+                  <i class="fa fa-file-text"></i>
+                  <span>Documents</span>
+                </a>
+              </li>
+              <li class="@yield('pages_active')">
+                <a href="{{ secure_url('/pages') }}">
+                  <!--b class="badge bg-info pull-right">3</b-->
+                  <i class="fa fa-envelope-o"></i>
+                  <span>E-Pages</span>
+                </a>
+              </li>
+              <li class="@yield('chair_active')">
+                <a href="{{ secure_url('/chair') }}">
+                  <i class="fa fa-tasks"></i>
+                  <span>Console</span>
+                </a>
+              </li>

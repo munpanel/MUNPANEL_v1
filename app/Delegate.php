a38287477f95421ffd6661564a61d2f21d946033
CRH380B-6216L <muner_szr6216@outlook.com>
2017-01-02 17:17:35 +0100

    搭档和室友检索

--- app/Delegate.php
+++ app/Delegate.php
@@ -74,4 +74,68 @@ class Delegate extends Model
         }
         return $result->unique()->sortBy('id');
     }
+    
+    public function assignPartnerByName() 
+    {
+        $this->partner_user_id = null;
+        if (isset($this->partnername)
+        {
+            $partner_name = $this->partnername;
+            $partners = User::where('name', $partner_name);
+            if (!is_null($partners)) // 对于带空格的partnername值，在此if表达式外增加foreach表达式以逐一处理
+            {
+                foreach ($partners as $partner)
+                {
+                    if ($partner->type != 'delegate') continue;                               // 排除非代表搭档
+                    if ($partner->delegate->committee != $this->committee) continue;          // 排除非本委员会搭档
+                    if (is_null($partner->delegate->partnername))                             // 如果对方未填搭档，自动补全
+                        $partner->delegate->partnername = $this->user->name;
+                    if ($partner->delegate->partnername != $this->user->name) continue;       // 排除多角搭档
+                    $this->partner_user_id = $partner->id;
+                    $partner->delegate->partner_user_id = $this->user->id;
+                    return;
+                }
+            }
+            $this->notes .= "在自动配对搭档时发生错误，请核查\n";
+            return;
+        }
+    }
+    
+    public function partner() {
+        return $this->hasOne('App\User', 'foreign_key')->delegate; // TODO: 确定这里的外键名称
+    }
+    
+    public function assignroommateByName() 
+    {
+        $this->roommate_user_id = null;
+        if (isset($this->roommatename)
+        {
+            $roommate_name = $this->roommatename;
+            $roommates = User::where('name', $roommate_name);
+            if (!is_null($roommates)) // 对于带空格的roommatename值，在此if表达式外增加foreach表达式以逐一处理
+            {
+                foreach ($roommates as $roommate)
+                {
+                    if ($roommate->type == 'unregistered') continue;                           // 排除未注册室友
+                    if (is_null($roommate->specific()->roommatename))                          // 如果对方未填室友，自动补全
+                        $roommate->specific()->roommatename = $this->user->name;
+                    if ($roommate->specific()->roommatename != $this->user->name) continue;    // 排除多角室友
+                    if ($roommate->specific()->gender != $this->gender)                        // 排除男女混宿
+                    {
+                        $this->notes .= "在自动配对室友时检测到室友为异性，请核查\n";
+                        return;
+                    }
+                    $this->roommate_user_id = $roommate->id;
+                    $roommate->specific()->roommate_user_id = $this->user->id;
+                    return;
+                }
+            }
+            $this->notes .= "在自动配对室友时发生错误，请核查\n";
+            return;
+        }
+    }
+    
+    public function roommate() {
+        return $this->hasOne('App\User', 'foreign_key')->specific(); // TODO: 确定这里的外键名称
+    }
 }

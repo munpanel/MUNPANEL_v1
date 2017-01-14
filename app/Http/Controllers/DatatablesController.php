f34edf31a52ba3e84bd91c6192eb62f55f3a862e
CRH380B-6216L <muner_szr6216@outlook.com>
2017-01-14 13:38:10 +0100

    继续转移 Controller 函数

--- app/Http/Controllers/DatatablesController.php
+++ app/Http/Controllers/DatatablesController.php
@@ -13,6 +13,7 @@ use App\Committee;
 use App\Assignment;
 use App\Handin;
 use App\Nation;
+use App\Good;
 use Config;
 use Illuminate\Support\Facades\Auth;
 
@@ -324,4 +325,48 @@ class DatatablesController extends Controller //To-Do: Permission Check
         return Datatables::of($result)->make(true);
     }
     
+    public function goodsList()
+    {
+        $result = new Collection;
+        $goods = Good::all();//get(['id', 'title', 'deadline']);
+        $i = 0;
+        foreach($goods as $good)
+        {
+            $remain = $good->remains;
+            if ($remain == -1) $remain = 99999;
+            if ($remain == 0) $command = '<p class="text-muted">已售罄</p>';
+            else
+            {
+                if (!$good->enabled) continue;
+                if (Auth::user()->type == 'ot')
+                {
+                    $command = 'TODO: 针对组委的操作项';
+                }
+                else
+                {
+                    $command = '数量： <div class="spinner input-group shop-spinner" id="MySpinner" data-max="' . $remain . '" data-min="1">
+                              <input name="spinner" class="form-control spinner-input" type="text" maxlength="2" value="1">
+                              <div class="btn-group btn-group-vertical input-group-btn">
+                                <button class="btn btn-white spinner-up" type="button">
+                                  <i class="fa fa-chevron-up text-muted"></i>
+                                </button>
+                                <button class="btn btn-white spinner-down" type="button">
+                                  <i class="fa fa-chevron-down text-muted"></i>
+                                </button>
+                              </div>
+                            </div> 　
+                  <a href="'.secure_url('/ot/committeeDetails.modal/new').'" class="btn btn-sm btn-success details-modal"><i class="fa fa-plus"></i> 加入购物车</a>';
+                }
+            }
+            $result->push([
+                'id' => ++$i, 
+                'image' => '<a href="good.modal/'. $good->id.'" data-toggle="ajaxModal"><img src="goodimg/' . $good->id . '" class="shop-image-small"></a>',
+                'title' => '<a href="good.modal/'. $good->id.'" data-toggle="ajaxModal">'.$good->name.'</a>',
+                'price' => $good->price,
+                'command' => $command,
+            ]);
+        }
+        return Datatables::of($result)->make(true);
+    }
+
 }

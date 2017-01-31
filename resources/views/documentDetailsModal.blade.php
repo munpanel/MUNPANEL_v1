53045c4333086d4fd663e02ee691c5ed1bc12270
CRH380B-6216L <muner_szr6216@outlook.com>
2017-01-31 15:21:47 +0100

    恢复被误修改的文件
    resources/views/documentDetailsModal.blade.php

--- resources/views/documentDetailsModal.blade.php
+++ resources/views/documentDetailsModal.blade.php
@@ -1,24 +1,64 @@
 ﻿<div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-body">
-            <h4>{{$delegate->user->name}}</h4>
-            <p>{{$delegate->user->school->name}}
-            <br>{{$delegate->committee->display_name}}
-            @if ($delegate->committee->language == 'ChineseS')
-                &nbsp;{{$delegate->nation->name}}代表
-            @else
-                , delegate of {{$delegate->nation->name}}
+            @if (Auth::user()->type == 'ot' || Auth::user()->type == 'dais') 
+                <button id="enableEditable-{{$document->id}}" class="btn btn-default pull-right">编辑模式</button><span class="pull-right">&nbsp;</span><button id="deleteButton-{{$document->id}}" class="btn btn-danger pull-right">删除</button>
             @endif
-            </p>
-            <p>
-            @if (isset($delegate->qq))
-                <i class="fa fa-qq"></i>&nbsp;{{$delegate->qq}}
-            @endif
-            @if (isset($delegate->wechat))
-                <i class="fa fa-wechat"></i>&nbsp;{{$delegate->wechat}}
-            @endif
-            <i class="fa fa-phone"></i>&nbsp;{{$delegate->tel}}
-            <i class="fa fa-email"></i>&nbsp;{{$delegate->user->email}}
+            <h4>{{(Auth::user()->type == 'ot' || Auth::user()->type == 'dais') ? '学术文件 #' . $document->id : $document->title}}</h4>
+            <table id="document-{{$document->id}}" class="table table-bordered table-striped" style="clear: both">
+                <tbody>
+                    @if (Auth::user()->type == 'ot' || Auth::user()->type == 'dais')
+                    <tr>
+                        <td width="35%">标题</td>
+                        <td width="65%"><a href="#" id="title" data-type="text" data-pk="{{$document->id}}" data-url="{{secure_url('/ot/update/document/'.$document->id)}}" data-title="title" class="editable">{{$document->title}}</a></td>
+                    </tr>
+                    @endif
+                    <tr>
+                        <td width="35%">分发对象</td>
+                        <td width="65%">{{$document->scope()}}</td>
+                    </tr>
+                    <tr>
+                        <td width="35%">创建日期</td>
+                        <td width="65%">{{$document->created_at}}</td>
+                    </tr>
+                    <tr>
+                        <td width="35%">描述</td>
+                        @if (Auth::user()->type == 'ot' || Auth::user()->type == 'dais')
+                        <!-- TODO: 改用多行文本 -->
+                        <td width="65%"><a href="#" id="description" data-type="text" data-pk="{{$document->id}}" data-url="{{secure_url('/ot/update/document/'.$document->id)}}" data-title="description" class="editable">{!!$document->description!!}</a></td>
+                        @else
+                        <td width="65%">{!!$document->description!!}</td>
+                        @endif
+                    </tr>
+                    <tr>         
+                        <td width="35%">统计信息</td>
+                        <td width="65%">{{$document->views}} 次阅览，{{$document->downloads}} 次下载</td>
+                    </tr>
+                    @if (Auth::user()->type == 'ot' || Auth::user()->type == 'dais')
+                    <tr>         
+                        <td width="35%">选择文件 (PDF 格式)</td>
+                        <td width="65%">{{$document->path}}<br>TODO: 文件选择器<br>TODO: 上传后重置统计信息</td>
+                    </tr>
+                    @endif
+                  </tbody>
+            </table>
         </div>
     </div>
 </div>
+@if (Auth::user()->type == 'ot' || Auth::user()->type == 'dais')
+<script>
+$.fn.editable.defaults.mode = 'inline';
+$('#document-{{$document->id}} .editable').editable();
+$('#document-{{$document->id}} .editable').editable('toggleDisabled');
+
+$('#enableEditable-{{$document->id}}').click(function() {
+    $('#document-{{$document->id}} .editable').editable('toggleDisabled');
+});
+$('#deleteButton-{{$document->id}}').click(function() {
+    var cb = function() {
+        $('#ajaxModal').modal('hide');
+    };
+    jQuery.get('ot/delete/document/{{$document->id}}', cb);
+});
+</script>
+@endif

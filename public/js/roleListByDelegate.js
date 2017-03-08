b9ac96f2b6ffb7f2533dd3acfce1d62ef0f89dfa
Adam Yi <xuan@yiad.am>
2017-03-09 03:19:45 +0800

    Fix bugs pertaining to modal

--- public/js/roleListByDelegate.js
+++ public/js/roleListByDelegate.js
@@ -13,16 +13,6 @@ $(document).ready(function() {
             {data: 'nation', name: 'nation', orderable: true},
         ],
         fnInitComplete: function(oSettings, json) {
-            $(document).on('click','.details-modal', function(e) {
-                $('#ajaxModal').remove();
-                e.preventDefault();
-                var $this = $(this)
-                  , $remote = $this.data('remote') || $this.attr('href')
-                  , $modal = $('<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>');
-                $('body').append($modal);
-                $modal.modal();
-                $modal.load($remote);
-            });
             $(document).on('hidden.bs.modal', '#ajaxModal', function() {
                 $('#nation-table').dataTable().fnReloadAjax(undefined, undefined, true);
             });

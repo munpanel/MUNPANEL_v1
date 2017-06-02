e2bec9d2bf949e9d813396c1633759810481e7db
Adam Yi <xuan@yiad.am>
2017-06-02 19:35:57 +0800

    bug fix

--- resources/views/selectIdentityModal.blade.php
+++ resources/views/selectIdentityModal.blade.php
@@ -7,7 +7,7 @@
        {{csrf_field()}}
        <div class="input-group input-m">
         <select name="reg" class="form-control m-b">
-        @foreach(Auth::user()->regs->where('conference_id', Reg::currentConferenceID())->where('enabled', true) as $reg)
+        @foreach(Auth::user()->regs()->where('conference_id', Reg::currentConferenceID())->where('enabled', true)->get() as $reg)
         <option value="{{$reg->id}}" {{ $reg->id == Reg::current()->id ? 'selected' : '' }}>{{$reg->regText()}}</option>
         @endforeach
         <option value="logout">注销用户</option>

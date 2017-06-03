284b6d96d20314597a4b850ff83a26d5a0d7665e
adamyi <xuan@yiad.am>
2017-06-03 15:57:50 +0800

    Cache domain routing

--- app/Http/Middleware/DomainRouting.php
+++ app/Http/Middleware/DomainRouting.php
@@ -14,6 +14,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+
 
 class DomainRouting
 {
@@ -30,16 +32,25 @@ class DomainRouting
         {
             if (is_null(config('munpanel.conference_id'))) //we may route all domains to one conference for debugging and developing.
             {
-                $conference_id = DB::table('domains')->where('domain', $_SERVER['HTTP_HOST'])->value('conference_id');
-                if (isset($conference_id))
-                    config(['munpanel.conference_id' => $conference_id]);
-                else
+                $domain = $_SERVER['HTTP_HOST'];
+                $conference_id = Cache::tags('domains')->get($domain);
+                if (!isset($conference_id))
                 {
-                    if (Auth::check())
-                        return redirect(route('portal'));
+                    $conference_id = DB::table('domains')->where('domain', $domain)->value('conference_id');
+                    if (isset($conference_id))
+                    {
+                        config(['munpanel.conference_id' => $conference_id]);
+                        Cache::tags('domains')->put($domain, $conference_id, 1440); // Takes a day for removing a domain to work.
+                    }
                     else
-                        return redirect(route('landing'));
-                }
+                    {
+                        if (Auth::check())
+                            return redirect(route('portal'));
+                        else
+                            return redirect(route('landing'));
+                    }
+                } else
+                    config(['munpanel.conference_id' => $conference_id]);
             }
         }
         return $next($request);
